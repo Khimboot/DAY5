a690fff3152bc322469e8aa0e943fcdd3797cde8
TENGKH <TENGKH@ZHA-ITA024-W7.corp.oocl.com>
2017-08-04 11:27:33 +0800

    Day 5 Git Practice First commit

---
+++ day5/src/main/java/com/oocl/training/gitPractice/Main.java
@@ -0,0 +1,11 @@
+package com.oocl.training.gitPractice;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hi");
+	}
+
+}
