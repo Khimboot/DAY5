fb181e9b2ac15d996e64c4f9da93d6a78fe02bfa
TENGKH <TENGKH@ZHA-ITA024-W7.corp.oocl.com>
2017-08-04 11:33:31 +0800

    Day 5 Git Practice
    Second commit:
    Modified codes

--- day5/src/main/java/com/oocl/training/gitPractice/Main.java
+++ day5/src/main/java/com/oocl/training/gitPractice/Main.java
@@ -5,7 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hi");
+
+		String a = "Hi";
+		System.out.println(a);
+		
 	}
 
 }
